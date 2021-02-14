030e82457495710498b7616c6bf8df50122edfaf
sivar2311 <sivar2311@googlemail.com>
2021-02-14 10:44:38 +0100

    fixed onKeystroke Callback (typo)

--- controller/KeypadController/callbacks.h
+++ controller/KeypadController/callbacks.h
@@ -1,5 +1,5 @@
 // KeypadController
-bool onKeyStroke(const String &deviceId, String &keystroke) {
+bool onKeystroke(const String &deviceId, String &keystroke) {
   Serial.printf("Key %s have been pressed\r\n", keystroke.c_str());
   return true;
 }
