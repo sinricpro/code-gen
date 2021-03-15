76fc95fd3981eadea822b9844af13780ac7f166a
sivar2311 <sivar2311@googlemail.com>
2021-03-15 06:11:19 +0100

    Added "onAdjustRangeValue" in setup

--- capabilities/RangeController/setup.h
+++ capabilities/RangeController/setup.h
@@ -1,3 +1,4 @@
   // RangeController
 <% rangeControllerInstances.forEach(element => {%>  <%=device.deviceInstance%>.onRangeValue("<%=element%>", onRangeValue);
+  <%=device.deviceInstance%>.onAdjustRangeValue("<%=element%>", onAdjustRangeValue);
 <%});%>
