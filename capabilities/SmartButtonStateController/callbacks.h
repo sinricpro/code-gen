2dddad9336316649f3fc6ba7503d59bc2fd98cfb
Aruna Tennakoon <aruna.tennakoon@gmail.com>
2024-11-11 11:52:29 +0700

    Update callbacks.h

--- capabilities/SmartButtonStateController/callbacks.h
+++ capabilities/SmartButtonStateController/callbacks.h
@@ -2,3 +2,11 @@
 bool onSinglePress(const String &deviceId) {
   return true; // request handled properly
 }
+
+bool onDoublePress(const String &deviceId) {
+  return true; // request handled properly
+}
+
+bool onLongPress(const String &deviceId) {
+  return true; // request handled properly
+}
