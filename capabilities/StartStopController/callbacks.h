0d5d8c5a9119bc4262ddc5ecba1a9c93142a472a
Aruna Tennakoon <aruna.tennakoon@gmail.com>
2025-02-09 15:35:50 +0700

    Update callbacks.h

--- capabilities/StartStopController/callbacks.h
+++ capabilities/StartStopController/callbacks.h
@@ -1,1 +1,10 @@
+// StartStopController
+bool onStartStop(const String& deviceId, bool &start) {
+  Serial.printf("Set to %s\r\n", start ? "start" : "stop");
+  return true;
+}
 
+bool onPauseUnpause(const String& deviceId, bool &pause) {
+  Serial.printf("Set to %s\r\n", pause ? "pause" : "unpause");
+  return true;
+}
