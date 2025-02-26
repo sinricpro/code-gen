57bbb65b2a074cab5c86b112617ff3567fe18fb3
Aruna Tennakoon <aruna.tennakoon@gmail.com>
2025-02-26 09:25:06 +0700

    Update callbacks.h

--- capabilities/OpenCloseController/callbacks.h
+++ capabilities/OpenCloseController/callbacks.h
@@ -1,5 +1,10 @@
 // OpenCloseController
-bool onOpenClose(const String& deviceId, const String& openDirection, int& openPercent) {
+bool onDirectionOpenClose(const String& deviceId, const String& openDirection, int& openPercent) {
   Serial.printf("Open direction to %s, percent %d\r\n", openDirection.c_str(), openPercent);
+  return true;
+}
+ 
+bool onOpenClose(const String& deviceId, int& openPercent) {
+  Serial.printf("Open percent %d\r\n", openPercent);
   return true;
 }
